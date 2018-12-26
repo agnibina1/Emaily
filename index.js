a0aff74069d7cb121af0b58eeb8ea6389c733d02
Agnibina Sengupta <agngupta@in.ibm.com>
2018-12-26 23:16:10 +0530

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=1006666243084-l9hres6ese3v3pu71vvi9pb9as95nnqg.apps.googleusercontent.com
+require("./services/passport");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send({ hi: "we changed" });
 });
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
